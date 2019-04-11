0f75b9603322b7ae4871978ecf38034ff32a6fa0
freebit <freebit77@gmail.com>
2019-04-11 10:27:00 +0300

    proxy conf

--- src/api/index.js
+++ src/api/index.js
@@ -1,22 +1,24 @@
 import http from 'axios'
 
 const apiClient = http.create({
-  baseURL: '/api'
+  baseURL: 'http://chat/api'
 })
 
 const sendMessage = async (message) => {
   let result = null
 
-  const formData = new FormData();
-  formData.set('q', message);
+  const formData = new FormData()
+  formData.set('q', message)
 
   await apiClient.post('/get-answer', formData, {
     crossDomain: true,
     headers: {
+      'Access-Control-Request-Method': '*',
+      'Access-Control-Request-Headers': 'X-Custom-Header',
       'Acces-Control-Allow-Origin': '*'
     }
   }).then(({ data }) => {
-    if (data.ok) {
+    if (data && data.ok) {
       result = data.a
     }
   }).catch(error => {
